a978ff7ac01bb1954fbd3389938e38d5e56c7268
HamzaWaqar82 <151508571+HamzaWaqar82@users.noreply.github.com>
2024-08-07 12:39:16 +0000

    new changes

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict"
+
+////////////////// Selecting Elements ////////////////////////////////////////
+const score0Element = document.querySelector('#total-score0');
+const score1Element = document.querySelector('#total-score1');
+
+score0Element.textContent = 0;
+score1Element.textContent = 0;
